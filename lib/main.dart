5a6a3dac76e04f1092436aa985881716598d1eca
Sofia <csp.santos@campus.fct.unl.pt>
2022-11-23 01:34:34 +0000

    Buttons hide when you open the drawer

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'white_theme.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
-
 void main() {
   runApp(const App());
 }
@@ -16,12 +15,11 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Time Garden',
-      theme: ThemeData(
-        primarySwatch: myWhite,
-      ),
-      home: const HomePage(title: 'Time Garden')
-    );
+        title: 'Time Garden',
+        theme: ThemeData(
+          primarySwatch: myWhite,
+        ),
+        home: const HomePage(title: 'Time Garden'));
   }
 }
 
@@ -34,85 +32,144 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+const double buttonSize = 50.0;
+final panelController = PanelController();
 
 class _HomePageState extends State<HomePage> {
-  static const double buttonHeightClosed = 75.0;
-  double buttonHeight = buttonHeightClosed;
+  static const Color iconColor = Color(0xFF383838);
+  static const double elevation = 0.7;
+  static const double initButtonPosition = buttonSize + 50.0;
+  static const Duration fadeTime = Duration(milliseconds: 200);
+  Color transparencyLvl = Colors.white.withOpacity(0.7);
+  double buttonPosition = initButtonPosition;
+  double padding = 15.0;
+  bool panelClosed = true;
 
-  final panelController = PanelController();
+  @override
+  void initState() {
+    super.initState();
+    buttonPosition = initButtonPosition;
+    panelClosed = true;
+  }
 
   @override
   Widget build(BuildContext context) {
     final panelHeightClosed = MediaQuery.of(context).size.height * 0.1;
     final panelHeightOpen = MediaQuery.of(context).size.height * 0.9;
+    padding = MediaQuery.of(context).size.height * 0.02;
 
-    return Scaffold(
-      backgroundColor: Colors.blue,  // will be removed
-      body: Stack(
-        alignment: Alignment.topCenter,
-        children: <Widget>[
-
-          // WIDGET A COPIAR PARA TER ACESSO AO DRAWER
-          SlidingUpPanel(
-            controller: panelController,
-            borderRadius: const BorderRadius.vertical(top: Radius.circular(15)),
-            maxHeight: panelHeightOpen,
-            minHeight: panelHeightClosed,
-            panelBuilder: (controller) => PanelWidget(
-              controller: controller,
-              panelController: panelController,
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          alignment: Alignment.topCenter,
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage('lib/help.png'),
+                ),
+              ),
             ),
-            onPanelSlide: (position) => setState(() {
-              final buttonPosition = panelHeightOpen - panelHeightClosed;
-              buttonHeight = position * buttonPosition + buttonHeightClosed;
-            }),
-          ),
-          // -----------------------------------
 
-          Positioned(
-            right: 15,
-            bottom: buttonHeight,
-            child: markerButton(context),
-          ),
-          Positioned(
-            left: 15,
-            bottom: buttonHeight,
-            child: FloatingActionButton(onPressed: () {}),
-          ),
-        ],
-      )
+            // WIDGET A COPIAR PARA TER ACESSO AO DRAWER
+            SlidingUpPanel(
+              controller: panelController,
+              borderRadius: const BorderRadius.vertical(top: Radius.circular(15)),
+              maxHeight: panelHeightOpen,
+              minHeight: panelHeightClosed,
+              panelBuilder: (controller) => PanelWidget(
+                controller: controller,
+                panelController: panelController,
+              ),
+              onPanelSlide: (position) => setState(() {
+                buttonPosition =
+                    position * (panelHeightOpen - panelHeightClosed) +
+                        initButtonPosition;
+                panelClosed = ((buttonPosition - initButtonPosition) == 0);
+              }),
+            ),
+            // -----------------------------------
+
+            Positioned(
+              right: padding,
+              bottom: buttonPosition,
+              child: AnimatedOpacity(
+                duration: fadeTime,
+                opacity: panelClosed ? 1 : 0,
+                child: markerButton(context),
+              ),
+            ),
+            Positioned(
+              left: padding,
+              bottom: buttonPosition,
+              child: AnimatedOpacity(
+                  duration: fadeTime,
+                  opacity: panelClosed ? 1 : 0,
+                  child: generalButton(context, Icons.settings)),
+            ),
+            Positioned(
+              left: padding,
+              bottom: buttonPosition + buttonSize + padding,
+              child: AnimatedOpacity(
+                  duration: fadeTime,
+                  opacity: panelClosed ? 1 : 0,
+                  child: generalButton(context, Icons.question_mark)),
+            ),
+            Positioned(
+              left: padding,
+              top: padding,
+              child: AnimatedOpacity(
+                  duration: fadeTime,
+                  opacity: panelClosed ? 1 : 0,
+                  child: generalButton(context, Icons.person)),
+            ),
+          ],
+
+        ),
+      ),
+    );
+  }
+
+  Widget generalButton(BuildContext context, IconData icon) {
+    return SizedBox(
+      height: buttonSize,
+      width: buttonSize,
+      child: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: transparencyLvl,
+        foregroundColor: iconColor,
+        elevation: elevation,
+        child: Icon(icon),
+      ),
     );
   }
 
   Widget markerButton(BuildContext context) {
-    Color transparencyLvl = Colors.white.withOpacity(0.7);
-
     return SpeedDial(
       overlayOpacity: 0,
       icon: Icons.location_on,
       backgroundColor: transparencyLvl,
-      foregroundColor: const Color(0xFF383838),
-      elevation: 0.4,
-      buttonSize: const Size(50.0, 50.0),
+      foregroundColor: iconColor,
+      elevation: elevation,
+      buttonSize: const Size(buttonSize, buttonSize),
       spacing: 7,
       children: [
         SpeedDialChild(
-          child: const Icon(Icons.search),
-          backgroundColor: transparencyLvl,
-          foregroundColor: const Color(0xFF383838),
-          elevation: 0.4,
-          onTap: () {}
-        ),
+            child: const Icon(Icons.search),
+            backgroundColor: transparencyLvl,
+            foregroundColor: iconColor,
+            elevation: elevation,
+            onTap: () {}),
         SpeedDialChild(
-          child: const Icon(Icons.add),
-          backgroundColor: transparencyLvl,
-          foregroundColor: const Color(0xFF383838),
-          elevation: 0.4,
-          onTap: () {}
-        ),
+            child: const Icon(Icons.add),
+            backgroundColor: transparencyLvl,
+            foregroundColor: iconColor,
+            elevation: elevation,
+            onTap: () {}),
       ],
     );
   }
-
-
 }
